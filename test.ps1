c6ee7f33b9b953d6a216457536c99b7f22db780d
Omoor <86531601+Omoorion@users.noreply.github.com>
2022-09-04 16:13:30 +0200

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,4 +1,4 @@
-# goes to your steam guard grabber file's location (you will have to insert your path).
-Set-Location D:\YourPath\steam-guard-grabber-main
+# goes to your steam guard grabber file's location (if the location is outside of the folder then you will have to insert your path).
+Set-Location steam-guard-grabber-main
 #executes index.js to write the new steam auth code into the text file, you also have to INSTALL NODEJS FOR THIS
 node.exe index.js
