8bb91c0446a94e8c92fc9139cb4c90367557c92d
Omoor <86531601+Omoorion@users.noreply.github.com>
2022-09-12 20:47:06 +0200

    Fixed a bug with the input_field

--- main.py
+++ main.py
@@ -145,7 +145,9 @@ while True:
     input_field = driver.find_element(By.NAME, "personaName")
 
     time.sleep(1)  # giving you one second to regret naming yourself something different
-
+    while input_field is None:
+        input_field = driver.find_element(By.NAME, "personaName")
+    driver.implicitly_wait(10)
     input_field.clear()
     time.sleep(1)  # for cool effect
     input_field.send_keys(name1)
