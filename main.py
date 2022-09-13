02a5495be2f5bb06c4a88a4d0900a55fb2f04a85
Omoor <86531601+Omoorion@users.noreply.github.com>
2022-09-13 15:57:13 +0200

    Fixed Input_field bug (again)

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import subprocess
 
 from selenium import webdriver
 from selenium.common import TimeoutException
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
